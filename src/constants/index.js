57059794929d02bf9ec63672076e2261394eddff
weimingye <weimingye1991@gmail.com>
2018-08-13 17:54:51 +0800

    111111

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,4 @@
  * Created by ybx on 2017/7/26.
  */
 // export const UrlHost = 'https://wgbapi.yituiyun.cn'    // 在线服务器
-export const UrlHost = 'http://192.168.1.12:8080'    // 测试服务
+export const UrlHost = 'http://192.168.0.22:8085'    // 测试服务
